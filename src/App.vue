c6737ef53a4f55cce6486110ee5eef9ee9961c2a
FengKaiYan <904634112@qq.com>
2019-09-19 11:47:14 +0800

    暂存修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <div>高级前端</div>
+    <div>高级前端99</div>
+    <div>特别饿</div>
+    <div>没有星星的夜里</div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 我也有
+// 木有
+// 木有
 Vue.config.productionTip = false
 
 new Vue({
